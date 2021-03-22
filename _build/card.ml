67ddcdb34f514de6b43aec199938d80ba6c6df28
Peter Munn <petermunn@Peters-MacBook-Pro-3.local>
2021-03-22 17:55:39 -0400

    implement deal

--- _build/card.ml
+++ _build/card.ml
@@ -2,7 +2,7 @@
 (* 0 is N, which always loses, and 14 is Wizard, which always wins*)
 type number = int 
 
-type suit = Red | Blue | Green | Yellow
+type suit = Red | Blue | Green | Yellow | No_Trump
 
 type card = {number : number; suit : suit }
 
@@ -11,3 +11,7 @@ type card_list = {cards : card list; size : int}
 let get_cards card_list = card_list.cards
 
 let get_cards_size card_list = card_list.size
+
+let set_cards card_list cards = {card_list with cards = cards}
+
+let make_no_trump () = {number = 0; suit= No_Trump}
--- card.mli
+++ card.mli
@@ -9,3 +9,7 @@ type card_list
 val get_cards : card_list -> card list
 
 val get_cards_size : card_list -> int
+
+val set_cards : card_list -> card list -> card_list
+
+val make_no_trump : unit -> card
