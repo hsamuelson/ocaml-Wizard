d6dce71d2b458d845c524c6c9ec74bb6edbd7420
Michelle Loven <ml2359@cornell.edu>
2021-03-26 16:45:38 -0400

    Adding main stuff

--- main.ml
+++ main.ml
@@ -1,18 +1,18 @@
 (*main should just print something in the terminal*)
 (*to compile and run, input "make play" *)
 
+(* >> brew install cloc >> cloc --by-file --include-lang=OCaml . *)
+
 open ANSITerminal
 
 (* play_game should print the table, and have a function to continually
    poll for inputs I guess, or to just type in inputs. Ask for number of
    players as the input. *)
 
-(* include unicode characters and colors to get suits
-0xE2 0x99 0xA4
-&#9828;
-&#x2664;*)
+(* include unicode characters and colors to get suits 0xE2 0x99 0xA4
+   &#9828; &#x2664;*)
 
-(** [play_game f] starts the adventure in file [f]. *)
+(* [play_game f] starts the adventure in file [f]. *)
 let play_game f : unit =
   print_string [ Bold ] ("you have selected: " ^ f ^ "\n\n");
   print_endline "Please enter the number of players.\n";
@@ -20,7 +20,7 @@ let play_game f : unit =
   match read_line () with
   | exception End_of_file -> ()
   | number_string ->
-    (**TODO: Catch error if inputting bad information for inputs?*)
+      (*TODO: Catch error if inputting bad information for inputs?*)
       let number = int_of_string number_string in
       if number > 0 && number <= 6 then
         print_string [ Bold ]
@@ -29,10 +29,11 @@ let play_game f : unit =
         print_string [ Bold ]
           "number of players must be at least 1 and at most 6"
 
-(** [main ()] prompts for the game to play, then starts it. *)
+(* [main ()] prompts for the game to play, then starts it. *)
 let main () =
   (*prompt for json file and number of players*)
-  ANSITerminal.print_string [ ANSITerminal.red ]
+  ANSITerminal.print_string
+    [ ANSITerminal.cyan; Bold ]
     "\n\nWelcome to the 3110 Wizard Game engine.\n";
   print_endline
     "Please enter the name of the deck json file you want to play with.\n";
