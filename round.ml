b09503b461e4fd02eefee9120a6ec88b206a4987
hsamuelson <hes227@cornell.edu>
2021-03-27 02:47:52 -0400

    Fixed bidding bug, there is still an compile error in play_round in my inital pattern match that I cannot solve... will try tommorrow

--- round.ml
+++ round.ml
@@ -1,6 +1,5 @@
 type t = {
   main_deck : Card.card_list;
-  remaining_tricks : int;
   players : Player.t list;
   num_players : int;
   round_num : int;
@@ -12,13 +11,12 @@ let init_first_round
     (plyrs : Player.t list) =
   {
     main_deck = dck;
-    remaining_tricks = 1;
     players = plyrs;
     num_players = p_num;
     round_num = 1;
   }
 
-  (* This function asks the usr for a bet *)
+(* This function asks the usr for a bet *)
 let usr_bet () = 
   match read_line () with 
   | exception End_of_file -> 0
@@ -26,21 +24,24 @@ let usr_bet () =
 
   (* This will run the bidding by going through all players
   Asking for their bet *)
-let rec run_bidding t_trck bet_sum plyrs = 
-  match plyrs with
-  | hd :: tl -> begin
-    let bet = usr_bet () in
-    if bet + bet_sum = t_trck then
-      (* Invalid bet *)
-      (* ignore (Printf.printf "Bet cannot sum to number of tricks!") *)
-      run_bidding t_trck bet_sum plyrs
-    else
-      (* In this case the bet was correct
-          - Assign bet to player 
-          - Move player to back of queue and ask next player*)
-      run_bidding t_trck (bet_sum + bet )(tl @ [Player.make_bet bet hd])
-  end
-  | _ -> failwith "Error in bidding"
+let rec run_bidding t_trck bet_sum num_p cntr plyrs = 
+  if cntr <= num_p then
+    match plyrs with
+    | hd :: tl -> begin
+      let bet = usr_bet () in
+      if bet + bet_sum = t_trck then
+        (* Invalid bet *)
+        (* ignore (Printf.printf "Bet cannot sum to number of tricks!") *)
+        run_bidding t_trck bet_sum num_p cntr plyrs
+      else
+        (* In this case the bet was correct
+            - Assign bet to player 
+            - Move player to back of queue and ask next player*)
+        run_bidding t_trck (bet_sum + bet ) num_p cntr (tl @ [Player.make_bet bet hd])
+    end
+    (* Not positive that this is an error yet *)
+    | _ -> failwith "Error in bidding"
+  else plyrs
 
 let rec assign_hands (players : Player.t list) hands =
   match players with
@@ -56,9 +57,10 @@ let rec assign_hands (players : Player.t list) hands =
 let play_round (rnd : t) =
   (* Shuffle Deck *)
   match Deck.shuffle rnd.main_deck |> Deck.deal with
-  | (hands, trump) -> begin
-    (* Assign Hands *)
-    assign_hands rnd.players hands
-  end
-  | _ -> failwith "There has been an Error in Deal"
+  | (hands, trump) -> hands
+  | _ -> []
+  (* Assign hands *)
+  |> assign_hands rnd.players
   (* We now run bidding. *)
+  |> run_bidding rnd.round_num 0 rnd.num_players 0
+  (* Now we *)
