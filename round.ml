f4614a7481fb3d32ff0ea4e68042eb23c3083685
hsamuelson <hes227@cornell.edu>
2021-03-27 02:07:00 -0400

    Working assign_hands function

--- round.ml
+++ round.ml
@@ -6,7 +6,10 @@ type t = {
   round_num : int;
 }
 
-let init_first_round (p_num : int)(dck : Card.card_list)(plyrs : Player.t list) =
+let init_first_round
+    (p_num : int)
+    (dck : Card.card_list)
+    (plyrs : Player.t list) =
   {
     main_deck = dck;
     remaining_tricks = 1;
@@ -15,22 +18,22 @@ let init_first_round (p_num : int)(dck : Card.card_list)(plyrs : Player.t list) 
     round_num = 1;
   }
 
-
 let rec assign_hands (players : Player.t list) hands =
-  match hands with
-  | hd::tl -> begin
-
-  end
+  match players with
+  | hd :: tl -> begin
+      match hands with
+      | fst_hand :: other_hands ->
+          assign_hands (tl @ [Player.give_cards fst_hand hd])
+        other_hands
+      | _ -> players
+      end
   | _ -> failwith "No hands were passed"
 
 let play_round (rnd : t) players =
-(* Shuffle Deck *)
-match Deck.shuffle rnd.main_deck
-|> Deck.deal with
-| (hands,trump) -> begin
-
-end
-| _ -> failwith "There has been an Error in Deal"
+  (* Shuffle Deck *)
+  match Deck.shuffle rnd.main_deck |> Deck.deal with
+  | hands, trump -> ()
+  | _ -> failwith "There has been an Error in Deal"
 
 (* Deal Cards to each player *)
 (* Distribute cards to each player *)
