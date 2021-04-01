90ca16ffd742b20db4262b6e47b7fe78a5aac135
Michelle Loven <ml2359@cornell.edu>
2021-03-31 21:49:40 -0400

    adding author pages

---
+++ authors.mli
@@ -0,0 +1,18 @@
+(** CS 3110 Spring 2021 Final Project: Wizard Card Game
+
+    @author Henry Samuelson (hes227)
+    @author Peter Munn (pcm82)
+    @author Michelle Loven (ml2359) *)
+
+(************************************************************ 
+
+   Academic Integrity Statement
+
+   I, the person named in the @author comment above, have fully reviewed the
+   course academic integrity policies.  I have adhered to those policies in
+   solving the assignment.
+
+ ************************************************************)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
