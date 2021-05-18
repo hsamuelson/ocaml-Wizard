c9a08ed1edb1a026acb1f6662aecb1692a3eef8f
Peter Munn <pcm82@cornell.edu>
2021-05-18 00:28:21 -0400

    add print_func

--- printFunct.mli
+++ printFunct.mli
@@ -1,3 +1,5 @@
+(**[intro_screen] prints the intro screen graphic for the wizard game*)
 val intro_screen : unit -> unit
 
+(**[print_hand] prints the graphic representing card list [c_list]*)
 val print_hand : Card.card list -> int -> unit
